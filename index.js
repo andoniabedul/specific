39903b56b57c7c2d0dc1e3b769ae67bbd954e4ab
Manuel de la Higuera <mhiguera@gmail.com>
2014-07-24 15:56:21 +0200

    Minor changes and improvements

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-var path    = require('path');
-var current = __dirname;
-var root    = path.resolve(current, '../..');
+var contextPath,
+    aliases = {},
+    path = require('path');
+    current = __dirname,
+    root = path.resolve(current, '../..');
+
 module.exports = {
   require: function(filepath) {
     return require(path.resolve(root, filepath));
   },
 
-  setRoot: function(r) {
-    root = r;
+  setContextPath: function(r) {
+    contextPath = r;
+    root = path.resolve(root, r);
   },
 
-  addRequireAlias: function(alias, directory) {
+  getPath: function(alias) {
+    return aliases[alias];
+  },
+
+  addPath: function(alias, directory) {
+    aliases[alias] = directory;
     var wrapper = this.require;
     this.require[alias] = function(filepath) {
-      return wrapper(path.resolve(directory, filepath));
+      return wrapper(path.resolve(directory, filepath || ''));
     }
   }
 }
