ea07f058d662dd399cb9e3e93620ad5369d405df
Daniel Eguzkiza <daniel.eguzkiza@biko2.com>
2023-03-30 21:10:25 +0200

    Simplified module resolution and fix not resolved modules handling

--- index.js
+++ index.js
@@ -1,123 +1,94 @@
 var contextPath,
-    cargo = {},
-    aliases = {},
-    path = require('path'),
-    current = __dirname,
-    commonDirectory = 'common',
-    resolverActive = false,
-    root = path.resolve(current, '../..');
-
-
-const _resolve = function (paths) {
-  return path.resolve(root, paths.join('/'))
-};
-
-const _isModule = function(path){ 
-  try {
-    require.resolve(path);
-    return true;
-  } catch (e) {
-    return false;
-  }
-}
-
-const _findModulePath = function(possibleModule, extension) {
-  const resolved = _resolve(possibleModule) + extension;
-  return _isModule(resolved) ? resolved : null;
-}
+  cargo = {},
+  aliases = {},
+  path = require("path"),
+  util = require("util"),
+  current = __dirname,
+  commonDirectory = "common",
+  resolverActive = false,
+  root = path.resolve(current, "../..");
 
 module.exports = {
-  require: function(filepath) {
+  require: function (filepath) {
     return require(path.resolve(root, filepath));
   },
 
-  set: function(varName, info) {
+  set: function (varName, info) {
     cargo[varName] = info;
   },
 
-  get: function(varName) {
+  get: function (varName) {
     return cargo[varName];
   },
 
-  setContextPath: function(r) {
+  setContextPath: function (r) {
     contextPath = r;
     root = path.resolve(root, r);
   },
 
-  getPath: function(directory) {
-    return path.resolve(root, directory || '');
+  getPath: function (directory) {
+    return path.resolve(root, directory || "");
   },
 
-  isModule: function(path){ 
-    try {
-      require.resolve(path);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  },
-  
-  resolve: function (paths) {
-    return path.resolve(root, paths.join('/'))
-  },
-  resolver: function(directory, filepath) {
-    const paths = filepath.split('/');
-    let possibleModules = [
-      [
-        paths.length === 1 ? commonDirectory : paths[0], 
-        directory, 
-        ...Array.from(paths).slice(paths.length === 1 ? 0 : 1)
-      ],
-      [
-        commonDirectory,
-        directory,
-        paths.join('/')
-      ],
-      [
-        paths[0], 
-        directory
-      ],
-    ];
-    const resolved = possibleModules.map((possibleModule) => {
-      return [
-        _findModulePath(possibleModule, '.js'),
-        _findModulePath(possibleModule, '/index.js'),
-        _findModulePath(possibleModule, ''),
-      ].find(Boolean);
-    }).find(Boolean);
-    if(!resolved){    
-      global.logger.error(
-        '[SPECIFIC ERROR] Not module found %s in %s',
-        filepath, 
-        directory
-      );
-      global.logger.info(
-        possibleModules.map(
-          (list) => list.map((path) => path.join('/'))
-        )
-      );
-    }
-    return resolved;
-  },
-
-  addPath: function(alias, directory, commonDir) {
+  addPath: function (alias, directory, commonDir) {
     aliases[alias] = directory;
-    const self = this;
-    var wrapper = this.require;
-    if(commonDir) commonDirectory = commonDir;
-    this.require[alias] = function(filepath) {
-      const resolved = resolverActive ? 
-        self.resolver(aliases[alias], filepath) :
-        path.resolve(root, directory, filepath || '');
+    const wrapper = this.require;
+    if (commonDir) {
+      commonDirectory = commonDir;
+    }
+    this.require[alias] = function (filepath) {
+      const resolved = resolverActive
+        ? resolver(aliases[alias], filepath)
+        : path.resolve(root, directory, filepath || "");
       return wrapper(resolved);
-    }
-
-    this.getPath[alias] = function(filepath) {
-      return path.resolve(root, directory, filepath || '');
-    }
+    };
+    this.getPath[alias] = function (filepath) {
+      return resolverActive
+        ? resolver(aliases[alias], filepath)
+        : path.resolve(root, directory, filepath || "");
+    };
   },
-  
-  setResolver: function(isActive){
+
+  setResolver: function (isActive) {
     resolverActive = isActive;
+  },
+};
+
+function resolver(directory, filepath) {
+  const [basePath, subPath] = filepath.split(/[/](.*)/, 2);
+  const possibleModules = [
+    [
+      subPath ? basePath : commonDirectory,
+      directory,
+      subPath ? subPath : basePath,
+    ],
+    [commonDirectory, directory, basePath, subPath],
+    [basePath, directory],
+  ].map((posibleModule) => posibleModule.join("/"));
+  const resolved = possibleModules.map(findModulePath).find(Boolean);
+  if (!resolved) {
+    const message = util.format(
+      "[SPECIFIC ERROR] Not module found %s in %s",
+      filepath,
+      directory,
+      possibleModules
+    );
+    const error = new Error(message);
+    error.code = "MODULE_NOT_FOUND";
+    throw error;
+  }
+  return resolved;
+}
+
+function findModulePath(posiblePath) {
+  const absolutePath = path.resolve(root, posiblePath);
+  return resolve(absolutePath);
+}
+
+function resolve(path) {
+  try {
+    return require.resolve(path);
+  } catch (_error) {
+    return undefined;
   }
 }
